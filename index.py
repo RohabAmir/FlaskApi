61f5190c7a85e75ca0768ce6cafece81b81c7887
Ahmad Kashif <ahmadkashif983@gmail.com>
2022-10-26 11:49:32 +0500

    server test run successful

--- index.py
+++ index.py
@@ -1,8 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
 def hello():
     return "Hello World!";
+
+@app.route("/mynetwork")
+def hello():
+    return "Hello mynetwork!";
+
+@app.route("/jobs")
+def hello():
+    return "Hello jobs!";
+
 
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0", port = 3000)
