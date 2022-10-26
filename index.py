8149c650d2a4ae459f4c5ca1164fc874571d3184
Ahmad Kashif <ahmadkashif983@gmail.com>
2022-10-26 12:03:39 +0500

    server test run successful

--- index.py
+++ index.py
@@ -1,18 +1,28 @@
+# import Flask 'module'
 from flask import Flask
+
+# this will make a server application
 app = Flask(__name__)
 
+# this will define resources according to the route.
 @app.route("/")
 def hello():
     return "Hello World!";
+
+# this will define resources according to the route.
 
 @app.route("/mynetwork")
 def mynetwork():
     return "Hello mynetwork!";
 
+# this will define resources according to the route.
 @app.route("/jobs")
 def jobs():
     return "Hello jobs!";
 
-
+#
 if __name__ == "__main__":
-    app.run(debug = True, host = "0.0.0.0", port = 3000)
+    app.run(
+    debug = True,
+    host = "0.0.0.0", # this defines that the application is to be run on the local machine only. Hence localhost
+    port = 3000) # this is the port number that the app runs on.
