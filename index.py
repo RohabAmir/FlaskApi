aef725c0d40e51770d3dd6f120978c9b63b75702
Ahmad Kashif <ahmadkashif983@gmail.com>
2022-10-27 11:30:25 +0500

    server test run successful

--- index.py
+++ index.py
@@ -24,8 +24,8 @@ n=4
 @app.route("/pattern")
 def getPattern():
     return n
-    for i in range (getPattern()):
-    print('*' * getPattern())
+    for i in range (n):
+    print('*' * n)
 
 
 if __name__ == "__main__":
